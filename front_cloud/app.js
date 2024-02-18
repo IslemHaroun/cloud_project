297b057ed092e687f900e2c9713687d96963583c
IslemHaroun <islemharoun@gmail.com>
2024-02-18 01:17:36 +0100

    Add new route

--- front_cloud/app.js
+++ front_cloud/app.js
@@ -8,8 +8,19 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
+
+app.get('/register', (req, res) => {
+    res.render('register');
+  });
+
+app.get('/user_home', (req, res) => {
+    res.render('user_home');
+  });
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
 app.get('/', (req, res) => {
-  // Rendre la vue principale d'accueil (home.ejs)
   res.render('home', { pageTitle: 'Accueil' });
 });
 // Démarrer le serveur
