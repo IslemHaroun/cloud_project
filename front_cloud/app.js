00c469d3851e50a1beab68dd3b0e0f69c1216b26
IslemHaroun <islemharoun@gmail.com>
2024-03-01 16:52:30 +0100

    Add img folder, add token_management, up navbar & user_home file

--- front_cloud/app.js
+++ front_cloud/app.js
@@ -7,6 +7,8 @@ app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use(express.static(path.join(__dirname, 'asset/img')));
+
 
 
 app.get('/register', (req, res) => {
@@ -18,9 +20,26 @@ app.get('/setting', (req, res) => {
 });
 
 app.get('/user_home', (req, res) => {
-
-    res.render('user_home');
+  fetch(`http://localhost:4000/system/os`, {
+      method: "GET",
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Erreur lors de la requête : ' + response.statusText);
+      }
+      return response.json();
+  })
+  .then(userData => {
+      const arrayVersion = Object.values(userData);
+      console.log(arrayVersion)
+      res.render('user_home', { data: arrayVersion });
+  })
+  .catch(error => {
+      console.error('Erreur lors de la requête :', error.message);
+      res.status(500).send('Erreur lors de la récupération des données');
   });
+});
+
 
 app.get('/login', (req, res) => {
     res.render('login');
@@ -29,7 +48,7 @@ app.get('/', (req, res) => {
   res.render('home', { pageTitle: 'Accueil' });
 });
 // Démarrer le serveur
-app.listen(3000, () => {
+app.listen({ port }, () => {
     console.log(`Serveur démarré sur le port ${port}`);
     console.log(`http://localhost:${port}`);
 });
